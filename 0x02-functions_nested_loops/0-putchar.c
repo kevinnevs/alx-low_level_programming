c67cda8b8b9420e93b51cad24f67964e286e849d
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-07 21:53:41 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On (1) Success.
- *
+ * Return: On Success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
