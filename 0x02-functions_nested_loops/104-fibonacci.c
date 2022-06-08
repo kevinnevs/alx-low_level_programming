4ff03d0b0e70c92e953a05be284f337f529b4863
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-08 11:25:15 -0700

    first 98 fibonacci num

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int counter = 2;
+
+	float a = 1;
+	float b = a +1;
+	float c = a + b;
+
+	printf("%.0f, ", a);
+	printf("%.of, ", b);
+	while (counter < 98)
+	{
+		counter++;
+		printf("%.0f", c);
+		a = b;
+		b = c;
+		c = a + b;
+		if (counter < 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
