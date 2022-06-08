0d9814486a22767bd6e822705216f795e90af292
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-08 10:54:17 -0700

    print 100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,21 @@
  */
 void print_times_table(int n)
 {
+	int i;
+	int j;
 
-	return (0);
-}
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int prod = j * i;
+
+				if (j == 0)
+				{
+					_putchar('0');
+				} else if (prod <= 9)
+				{
+				
+
