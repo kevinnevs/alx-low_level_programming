fa04066618ce605a48e256a4febb9da425fee2ad
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-09 17:09:26 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper (int c);
+/**
  * _isdigit -  a function that checks for a digit 0-9
  * @c: input
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isupper (int c);
 int _isdigit (int c);
 
 #endif
