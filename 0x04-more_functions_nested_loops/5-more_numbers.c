b65998b67726116adf58868b5474a8101430c768
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-10 00:09:42 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 {
 	int i, ro;
 
-	for (ro = 0; ro < 10; ro ++)
+	for (ro = 0; ro < 10; ro++)
 	{
-		for(i = 0; i <= 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
 			{
