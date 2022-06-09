204b8a9485e99a3942329f3683ea54c0c138366f
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-10 00:58:45 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
 /**
 * main - tests function that prints if integer is positive or negative
@@ -7,10 +5,8 @@
 */
 int main(void)
 {
-	int i, n;
+	int i;
 	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	i = 0;
 	positive_or_negative(i);
 
