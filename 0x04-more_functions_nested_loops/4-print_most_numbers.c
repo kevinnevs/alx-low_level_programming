d18f087112645fd763b7109cf3fc6f0159551824
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-09 22:10:11 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,14 @@
  */
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != '2' && i != '4')
-			_putchar(i);
+		if ((i == 2) || (i == 4)
+		    continue;
+		else
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
