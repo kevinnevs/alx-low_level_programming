d5d80f778cb3752fbe8217fbae2a705bf5878346
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-13 13:03:41 -0700

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#imclude "main.h"
+/**
+ * print_rev - a function that print s a string in reverse
+ * fcounter is first count to end, n is to count back
+ * @s: str input
+ * Return: returns string in reverse
+ */
+void print_rev(char *s)
+{
+	int fcounter = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		fcounter++;
+	}
+
+	for (n = (fcounter -1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
