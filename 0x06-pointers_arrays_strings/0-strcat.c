cc8147e1fb97e26eea8062ca6d26aa864fa6bd87
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-15 20:47:36 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,11 @@ char *_strcat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; dest[i] != '\0'; j++)
 	{
 	}
 
-	for (j = 0; j < n && src[i] != '\0'; j++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[j + i] = src[i];
 	}
