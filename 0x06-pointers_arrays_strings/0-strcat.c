5364168414843a972381466f0579e53d86e4a8fb
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-15 20:44:46 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,12 +2,12 @@
 /**
  * _strcat - this is a function that concatenates two string
  *
- * @dest - destination string
- * @src - source string
- *
+ * @dest: destination string
+ * @src: source string
+ * @n: amount of times to append 
  * Return: returing a pointer to the resulting string (dest)
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
@@ -16,7 +16,7 @@ char *_strcat(char *dest, char *src)
 	{
 	}
 
-	for (j = 0; src[i] != '\0'; j++)
+	for (j = 0; j < n && src[i] != '\0'; j++)
 	{
 		dest[j + i] = src[i];
 	}
