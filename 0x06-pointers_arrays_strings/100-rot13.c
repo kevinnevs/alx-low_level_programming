5b788dc3d2087c3dff3d11f75de526e92ff86c25
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-16 11:45:33 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - a function that encodes a string using rot13
+ * @s : the string to encrypt
+ *
+ * Return : returing the encrypted string
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+
+	char A[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char B[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; A[j] != '\0'; j++)
+		{
+			if (s[i] == A[j])
+			{
+				s[i] = B[j];
+			}
+		}
+	}
+	return (s);
+}
+
