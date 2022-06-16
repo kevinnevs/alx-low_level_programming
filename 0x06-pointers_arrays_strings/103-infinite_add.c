98c280b982cbda96d43823bc64047b5584083629
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-17 00:26:30 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,9 +19,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			if (n1 && n2 != '0' || size_r[j] != '\0')
 			{
 				r = n1 + n2;
-				break;
+				return (r);
+			} 
+			else
+			{
+				return (0);
 			}
 		}
 	}
-	return (r);
 }
