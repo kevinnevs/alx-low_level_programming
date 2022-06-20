83d8318b8a33357ad7dcd4b349b711ed275beb07
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-20 12:33:52 -0700

    copying mem area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copies from source to dest values in array
+ * @des: destination to copy to
+ * @src: source to copy from
+ * @n: the n amount of times
+ * Return: returns the string value(s)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
