66e92d9062eb38d8a20c01a9eb82009e54842b42
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-22 23:49:13 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,7 +14,7 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 != '\0')
 		return (0);
-	if (*s2 == '*');
+	if (*s2 == '*')
 		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == *s2)
 		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
