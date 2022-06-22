38970b1c4220cffeef0214ab06ce26961e4d6389
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-22 23:50:34 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -32,7 +32,7 @@ int wildcmp(char *s1, char *s)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == *s2)
-		retun (wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	else if (*s2 == '*')
 		return (substring_match(s1, (s2 + 1), (s2 + 1)));
 	else
