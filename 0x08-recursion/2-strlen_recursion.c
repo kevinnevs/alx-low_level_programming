b7dca0e86a59515db4729472c15008aedb4be561
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-22 12:07:56 -0700

    recursion length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,6 +2,7 @@
 /**
  * _strlen_recursion - a function that returns the length of a string
  * @s: the string to check the length
+ * Return: the length of a string 
  */
 int _strlen_recursion(char *s)
 {
