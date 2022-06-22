abb9deef3d5ea5443755ce56e9d4b8b135731d93
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-22 22:24:09 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,15 +8,15 @@ int factiorial(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	if (n == 0) 
 	{
-		return 1;
+		return (1);
 	}
 	if (n == 1)
 	{
-		return 1;
+		return (1);
 	}
 	return n * factorial(n - 1);
 }
