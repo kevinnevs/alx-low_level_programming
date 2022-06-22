f38fefada9c9004d9f0e0b80e029cea0364b7fd5
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-22 12:51:35 -0700

    square root recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * checker - helps check if i'm right
+ * @i: integer to guess
+ * @n: integer to get root of
+ * Return: value of root
+ */
+int checker (int i, int n)
+{
+	int j;
+
+	if (i * i != n)
+	{
+		if (i > n)
+		{
+			return (-1);
+		}
+		j = checker(i + 1, n);
+		return (j + 1);
+	}
+	return (0);
+}
+/**
+ * _sqrt_recursion - returns square root
+ * @n: integer to return
+ * Return: returns int of squareroot
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (checker(i, n) == n && n != 1)
+		return (-1);
+	return (checker (i, n));
+}
+
