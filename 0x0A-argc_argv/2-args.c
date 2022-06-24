9f238c5d9cffacd95ca1664962523a022912edb4
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-24 13:04:50 -0700

    print all program arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - the main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always (0) Success
+ */
+int main(int argc, __attribute__((unused)), char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
