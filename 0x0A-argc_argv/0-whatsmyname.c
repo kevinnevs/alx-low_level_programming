eeed3742827a45fb7568b948ba5f6af0d59821c2
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-24 12:45:24 -0700

    printing program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - this is the main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always (0) Success
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
+}
