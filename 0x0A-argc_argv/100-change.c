156c92cc695274f13153ce301fc270d768efe040
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-25 04:09:54 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,66 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
-int change(int cents);
+
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - prints the minimum number of coins for an amount of money
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always (0) if negative amount, (1) if amount not given
  */
+
 int main(int argc, char *argv[])
 {
+	int n, coins = 0;
+
+	/* validate input */
 	if (argc != 2)
 	{
-		printf("%s\n", "Error");
+		printf("Error\n");
 		return (1);
 	}
-	else if (argc < 0)
+
+	if (argv[1][0] == '-')
 	{
+		printf("0\n");
 		return (0);
 	}
 
-	printf("%d\n", change(atoi(argv[1])));
+	/* convert string to int and calculate coins */
+	n = atoi(argv[1]);
+
+	coins += n / 25;
+	n = n % 25;
+	coins += n / 10;
+	n = n % 10;
+	coins += n / 5;
+	n = n % 5;
+	coins += n / 2;
+	n = n % 2;
+	coins += n / 1;
+
+	printf("%d\n", coins);
 	return (0);
-}
-
-/**
- * change - get change
- * @cents: amount of coins from main function
- * Return: change
- */
-int change(int cents)
-{
-	int q = 25, d = 10, n = 5, t = 2, p = 1;
-	int coins;
-
-	while (cents > 0)
-	{
-		while (cents >= q)
-		{
-			cents -= q;
-			coins++;
-		}
-		while (cents >= d)
-		{
-			cents -= d;
-			coins++;
-		}
-		while (cents >= n)
-		{
-			cents -= n;
-			coins++;
-		}
-		while (cents >= t)
-		{
-			cents -= t;
-			coins++;
-		}
-		while (cents >= p)
-		{
-			cents -= p;
-			coins++;
-		}
-	}
-	return (coins);
 }
