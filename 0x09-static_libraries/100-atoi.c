1b69a738b77156a4e00d48ec6291eabe2aef3680
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-25 03:42:40 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,11 +1,59 @@
 #include "main.h"
+/**
+ * _strlen - gets character string
+ *
+ * @s: character to getstring
+ * Return: returns length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
 
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - converts string to integer
+ * @s: string to convert
+ *
+ * Return: returns integer value
  */
 int _atoi(char *s)
 {
-	return (0);
+	int i;
+	int np = 0;
+	int c;
+	int d = 1;
+	int num = 0;
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
+			break;
+		if (s[i] == '-')
+			np--;
+		if (s[i] == '+')
+			np++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			c++;
+		}
+	}
+	while (c > 0)
+	{
+		num += ((s[i - 1] - '0') * d);
+		i--;
+		c--;
+		d *= 10;
+	}
+	if (np >= 0)
+	{
+		num *= 1;
+	} else
+	{
+		num *= -1;
+	}
+	return (num);
 }
