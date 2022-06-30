6fad4b177124486dd6bacdf09d79e31d4910a147
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-06-30 22:41:30 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,40 +5,44 @@
  * string_nconcat - a func that concats strings
  * @s1: the first string
  * @s2: the second string
- * @n: the na amout of bytes
+ * @n: s2 no of bytes
  * Return: return the char val
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len1, len2;
-	char *t;
+	char *arr;
+	unsigned int i, j, co, co_2;
 
-	if (s2 == NULL)
-		s2 = "";
 	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	nel1 = 0;
-	nel2 = 0;
-	while (s2[len2] != '\0')
-		nel2++;
-	while (s1[len1] != '\0')
-		nel1++;
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
 
-	if (n >= len2)
-		n = nel2;
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
 
-	s = maloc(sizeof(char) * n + len1 + 1);
-	if (s == NULL)
+	if (n < j)
+		j = n;
+
+	j += i;
+	arr = malloc(sizeof(char *) * (j + 1));
+
+	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
-		s[i] = s1[i];
-
-	for (i = 0; i < n; i++)
-		s[i + len1] = s2[i];
-
-	s[i + len1] = '\0';
-
-	return (s);
+	for (co = 0; co < i; co++)
+		arr[co] = s1[co];
+	for (co_2 = 0; co < j; co_2++)
+	{
+		arr[co] = s2[co_2];
+		co++;
+	}
+	co++;
+	arr[co] = '\0';
+	return (arr);
 }
