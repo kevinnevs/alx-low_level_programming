0448ad18459ec46ec196761f34affd48db2c2ca9
kevinnevs <kevinnjuguna2@gmail.com>
2022-06-30 12:20:17 -0700

    Checking malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - a func that checks malloc
+ * @b: integer to check
+ * Return: a pointer or void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *t;
+
+	t = malloc(b);
+	if (t == NULL)
+		exit(98);
+
+	return (t);
+}
