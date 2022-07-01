5df5db47e9e7c058f1b9e43d4b0aa0108fbb53c4
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-01 16:56:58 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -119,7 +119,7 @@ int main(int argc, char *argv[])
 			_putchar(e[ti]);
 		exit(98);
 	}
-	init (a, ln - 1);
+	init(a, ln - 1);
 	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
 		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
