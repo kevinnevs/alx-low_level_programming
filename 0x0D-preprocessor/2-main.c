b32ce0453f3e15517864ecf458e2a18fc7dd1459
kevinnevs <kevinnjuguna2@gmail.com>
2022-07-04 13:32:31 -0700

    macro __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the main file returns file name
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
