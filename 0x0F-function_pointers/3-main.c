9b00d473cf5afd94905524fe242750a677f49079
kevinnevs <kevinnjuguna2@gmail.com>
2022-07-06 15:34:53 -0700

    perform simple operatin funct pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - the main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: the results
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == )
+	{
+		int a;
+		int b;
+		int (*func)(int, int);
+
+		a = atoi(argv[1]);
+		b = atoic(argv[3]);
+		func = get_op_func(argv[2]);
+		if ((*argv[2] == "%" || *argv[2] == "/") && b == 0)
+		{
+			printf("Error\n")
+				exit(100);
+		}
+		printf("%d\n", func(a, b));
+		return (0);
+	}
+	printf("Error\n");
+	exit(98);
+}
+				
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - a funct that returns sum of a & b
+ * @a: first int
+ * @b: second int
+ * Return: Always (0) Success
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - a funct that subtracts a & b
+ * @a: first int
+ * @b: second int
+ * Return: Always (0) Success
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - a funct that returns product of a & b
+ * @a: first int
+ * @b: second int
+ * Return: Always (0) Success
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - a funct that divides a & b
+ * @a: first int
+ * @b: second int
+ * Return:Always (0) Success
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/** 
+ * op_mod - a funct that returns modulus of a & b
+ * @a: first int
+ * @b: second int
+ * Return: Always (0) Success
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
