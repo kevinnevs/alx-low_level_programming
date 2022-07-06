a9ffc18c07e649fc3734c428dd25ee988c01a4ba
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-07 01:39:33 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		func = get_op_func(argv[2]);
 		if ((*argv[2] == "%" || *argv[2] == "/") && b == 0)
 		{
-			printf("Error\n")
+			printf("Error\n");
 				exit(100);
 		}
 		printf("%d\n", func(a, b));
