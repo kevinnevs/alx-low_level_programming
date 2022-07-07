8ac85454f880d7e714701cfeabf001cfe614f6d6
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-07 22:51:35 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,11 +5,11 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 int print_c(va_list a);
 int print_i(va_list a);
 int print_f(va_list a);
 int print_s(va_list a);
-void print_all(const char * const format, ...);
 /**
  * struct op - struct for 3
  * @c: character to check
