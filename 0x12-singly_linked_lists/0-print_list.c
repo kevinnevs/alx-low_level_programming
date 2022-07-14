b82358702880f1ae7ac685a2f4079fecf2d1bdee
kevinnevs <kevinnjuguna2@gmail.com>
2022-07-14 15:12:49 -0700

    print elem linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+#include <stddef.h>
+/**
+ * print_list - function to print elem  of list_t
+ * @h: head of list
+ * Return: the elem in the list
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str = NULL)
+		{
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+		}
+	}
+	return (count);
+}
+
+
