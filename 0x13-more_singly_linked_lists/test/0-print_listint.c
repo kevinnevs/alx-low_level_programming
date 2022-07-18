ca298813c40ed2a9e108bba3b9d2c0f3fd3682c7
kevinnevs <kevinnjuguna2@gmail.com>
2022-07-18 12:36:10 -0700

    testing codes

---
+++ 0x13-more_singly_linked_lists/test/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * print_listint - Entry Point
+ * @h: struct listint_t type
+ * Return: size_t value
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
