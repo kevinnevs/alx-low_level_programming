8a215bf7802bcdffdc5329d62c18d6b4bb1fc912
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-18 22:16:36 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,6 +17,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 
