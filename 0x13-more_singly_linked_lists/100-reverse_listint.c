9c58efe045c16d881631a7621e487d4084ad95e2
kevinnevs <kevinnjuguna2@gmail.com>
2022-07-19 13:23:04 -0700

    reverse node

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * reverse_listint - a func that reverse list
+ * @head: head
+ * Returns: a pointer to the fist node of reversed list
+ */
+listint_t *reverse_listint(listint_t **head);
+{
+	listint_t *copy;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (NULL);
+
+	copy = *head;
+	temp = NULL;
+
+	while (*head != NULL)
+	{
+		copy = (*head)->next;
+		(*head)->next = temp;
+		temp = *head;
+		*head = copy;
+	}
+
+	*head = temp;
+	return (*head);
+}
+
