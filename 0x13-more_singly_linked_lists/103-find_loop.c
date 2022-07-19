cdf4c15182d4e226b90f7b10222512745a305323
kevinnevs <kevinnjuguna2@gmail.com>
2022-07-19 14:35:06 -0700

    loop in linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * find_listint_loop - a func that finds loop in linked list
+ * @head: head
+ *
+ * Return: address of node where loop starts, NULL if no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+
