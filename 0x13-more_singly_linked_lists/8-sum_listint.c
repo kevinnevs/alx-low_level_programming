8cf9de4b8db1d1e6a08aa3adc7f83efab36e1cd2
kevinnevs <kevinnjuguna2@gmail.com>
2022-07-19 12:08:10 -0700

    ading nodes

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_listint - a func that adds node data
+ * @head: head
+ * 
+ * Return: Always (0) Success
+ */
+int sum_listint(listint_t *head)
+{
+	int total = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	
+	while (head != NULL)
+	{
+		total += head->n;
+		head = head->next;
+	}
+	return (total);
+}
