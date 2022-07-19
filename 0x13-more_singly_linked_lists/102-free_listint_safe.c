bb3b53bf558b828fb05feeec4af972c57fa5a4a9
kevinnevs <kevinnjuguna2@gmail.com>
2022-07-19 14:20:57 -0700

    free node safe mode

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * free_listint_safe - a func that frees listint_t (safe version)
+ * @h: linked list of type listint_t
+ *
+ * Return: no of nodes in freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+
