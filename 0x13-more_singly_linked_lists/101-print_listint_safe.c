6597eae3d04367e4648a75374bcb480c6d287b77
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-20 00:13:52 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * count_nodes_till_loop - count nodes to know now many unique nodes to print
+ * print_listint_safe - print a list
  * nodes to print
  * @head: head
  * Return: no of unique nodes in list before a loop
