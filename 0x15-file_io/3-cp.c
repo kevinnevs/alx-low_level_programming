661d4ba9e76ece2b1a592ac5be1c0b3f8633897f
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-26 01:03:45 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	file_from = open(argc[1], O_RDONLY);
+	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
