04aed8317c52b8fae4537442e4f6ef3f0d0a6717
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-26 00:18:54 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,24 +6,29 @@
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
-{
-	int fp, len = 0;
-	long int wrote;
+{{
+	int file, i = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fp == -1)
+
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (file == -1)
 		return (-1);
-	if (text_content != NULL)
+
+	while (text_content[i])
+		i++;
+
+	if (text_content == NULL)
 	{
-		while (text_content[len])
-			len++;
-		wrote = write(fp, text_content, len);
-		if (wrote == -1)
-			return (-1);
-	}
-	if (close(fp) - 1)
+		close(file);
 		return (-1);
+	}
+	else
+	{
+		write(file, text_content, i);
+	}
+
+	close(file);
 	return (1);
 }
