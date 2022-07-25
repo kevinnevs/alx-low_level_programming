577f5b8aff2a6a10e8274a0d7073506fe161f268
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-07-26 01:02:21 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@ void closer(int arg_files);
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_ro, file_from_r, wr_err;
+	int file_from, file_to, file_from_r, wr_err;
 	char *buf[1024];
 
 	if (argc != 3)
