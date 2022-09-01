14acad57bdd12b9de519d38d32d7fa84c94cd9cd
kevinnevs <kevinnjuguna2@gmail.com>
2022-09-01 13:59:12 -0700

    initial commit

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - func that deletes nodde at index
+ * @head: head
+ * @index: index
+ *
+ * Return: 1 if success, -1 if failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *temp = NULL;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	while (temp->next != NULL)
+	{
+		if (count == index)
+		{
+			temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			free(temp);
+			return (1);
+		}
+		temp = temp->next;
+		count++;
+	}
+
+	return (-1);
+}
