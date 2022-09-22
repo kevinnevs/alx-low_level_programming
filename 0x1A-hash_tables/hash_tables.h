60635bbdb4cc47ed1c72b566573d9fc60c1e579e
kevinnevs <kevinnjuguna2@gmail.com>
2022-09-22 13:42:31 -0700

    initial commit

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,5 +40,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 /* from 2-key_index.c */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+/* from 3-hash_table_set.c */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif
