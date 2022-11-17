3166a4ee80abb29e475a42cd5cb22830f1168e23
kevinnevs <kevinnjuguna2@gmail.com>
2022-11-17 09:09:51 -0800

    initial commit

--- binary_trees/9-binary_tree_height.c
+++ binary_trees/9-binary_tree_height.c
@@ -8,6 +8,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t left = 0, right = 0;
+
     if (tree == NULL || (tree->left == NULL && tree->right == NULL))
             return (0);
 
