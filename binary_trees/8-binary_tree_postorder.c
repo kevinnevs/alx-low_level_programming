f5f6b7687bfc80a54da33a2b3d3723ebcc9f6a84
kevinnevs <kevinnjuguna2@gmail.com>
2022-11-17 08:54:56 -0800

    initial commit

--- binary_trees/8-binary_tree_postorder.c
+++ binary_trees/8-binary_tree_postorder.c
@@ -7,12 +7,12 @@
  * Return: if tree or func is NULL, do nothing
  */
 
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL)
             return;
 
     binary_tree_postorder(tree->left, func);
-    func(tree->n);
     binary_tree_postorder(tree->right, func);
-})
+    func(tree->n);
+}
